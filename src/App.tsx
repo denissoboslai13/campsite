32dd33bf504754386ff90a0805450d64ed2dc6d8
denissoboslai13 <soboslaid@gmail.com>
2026-07-28 16:27:25 +0200

    ver 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,147 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+
+import Hamburger from './assets/icon-hamburger.svg?react'
+import flagSK from './assets/flags/sk.webp'
+import ChevronDown from './assets/icon-chevron-down.svg?react'
+import placeholderImg from './assets/placeholder.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+const logoText = 'YaCHT CaMPING'
+const images = [1,1,1,1,1,1,1,1,1,1,1,1]
+const tableData = [
+  {
+    items: [
+      { service: 'xyz', price: '10' },
+      { service: 'xyz', price: '12' },
+      { service: 'xyz', price: '15' },
+      { service: 'xyz', price: '20' },
+      { service: 'xyz', price: '25' }
+    ]
+  }
+]
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 bg-white z-100 w-full flex flex-row items-center justify-between p-4'>
+      <div className='font-["Big_Shoulders"] font-bold -skew-x-7 text-xl'>
+        {logoText.split('').map((e, i) => (
+          <span key={i} className={`m-0 ${e.toLowerCase() === 'a' ? 'text-blue-800 lowercase text-lg' : 'text-black'}`}>
+            {e}
+          </span>
+        ))}
+      </div>
+      <div className='flex flex-row items-center gap-10'>
+        <div className='flex flex-row items-center gap-1.5 py-1 px-2'>
+          <img src={flagSK} alt="" className='w-8'/>
+          <p>SK</p>
+          <ChevronDown />
+        </div>
+        <Hamburger />
+      </div>
+    </header>
+  )
+}
+
+const Hero = () => {
+  return (
+    <section className='bg-blue-800 w-full h-[256px] text-white flex flex-col items-center justify-center text-center px-6 gap-1'>
+      <h1 className='font-bold text-lg'>Üdvözlünk a Yacht-Camping világában</h1>
+      <p className='text-sm font-light'>Ahol vízi kaland és a természetközeli pihenés találkozik!</p>
+    </section>
+  )
+}
+
+const Showcase = () => {
+  return (
+    <section className='px-4 py-16'>
+      <div className='grid grid-cols-2 grid-rows-5 gap-y-8 gap-x-4'>
+        {images.map((e, i) => (
+          <span>
+            <img src={placeholderImg} alt="" />
+          </span>
+        ))}
+      </div>
+      <div className='flex justify-center pt-18'>
+        <div className='relative w-full aspect-video'>
+          <iframe 
+            src="https://www.youtube.com/embed/NpEaa2P7qZI" 
+            title="video placeholder" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowFullscreen
+            loading='lazy'
+            className='absolute inset-0 w-full h-full'
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const PriceList = () => {
+  return (
+    <section className='py-12 pb-24 flex flex-col items-center'>
+      <h2 className='font-bold text-3xl mb-12'>Cenník</h2>
+      <table className='table-auto border-collapse border'>
+        <tr>
+          <th className='border py-3 p-4 pr-16'>Service</th>
+          <th className='border py-3 p-4 pr-16'>Cena</th>
+        </tr>
+        {tableData[0].items.map((item) => (
+          <tr>
+            <td className='border py-3 p-4'>{item.service}</td>
+            <td className='border py-3 p-4'>{item.price}</td>
+          </tr>
+        ))}
+      </table>
+    </section>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <main className='min-w-[375px] font-[Asap]'>
+      <Header />
+      <Hero />
+      <Showcase />
+      <PriceList />
+      <section className='bg-blue-800 w-full py-12 pb-8 px-6 flex flex-col gap-10'>
+        <div className='flex flex-col gap-3 items-center'>
+          <span className='text-white flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faLocationDot}/>
+            <p>Chľaba 752</p>
+          </span>
+          <span className='text-white flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faPhone}/>
+            <p>+421 915 705 498</p>
+          </span>
+          <span className='text-white flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faEnvelope}/>
+            <p>yachtcamping1@gmail.com</p>
+          </span>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div className='flex justify-center border-3 border-white'>
+          <div className='relative w-full aspect-video'>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21430.21820753928!2d18.803142899999997!3d47.8245335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476a890023d8af8b%3A0x130435ac01181746!2zQ2FtcCBDaMS-YWJhLUtvdsOhxI1vdg!5e0!3m2!1ssk!2ssk!4v1785247708408!5m2!1ssk!2ssk" 
+              width="600" 
+              height="450"  
+              loading="lazy" 
+              referrerpolicy="strict-origin-when-cross-origin"
+              className='absolute inset-0 w-full h-full'
+            ></iframe>
+          </div>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    </main>
   )
+
 }
 
 export default App
