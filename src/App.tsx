93ab3ca455cb9bc72885b087f7ded1f5ddf65067
denissoboslai13 <soboslaid@gmail.com>
2026-08-03 17:25:23 +0200

    desktop

--- src/App.tsx
+++ src/App.tsx
@@ -252,15 +252,15 @@ const images = [
 const Header = ({ setNavVis, language, languageSelectVis, setLanguageSelectVis, handleLanguageSelect, isMobile, sections, handleNavClick }) => {
   return (
     <header className='sticky top-0 bg-white z-100 w-full flex flex-row items-center justify-between p-4 lg:px-8'>
-      <div className='font-["Big_Shoulders"] font-bold -skew-x-7 text-xl'>
+      <div className='font-["Big_Shoulders"] font-bold -skew-x-7 text-xl lg:text-2xl'>
         {logoText.split('').map((e, i) => (
-          <span key={i} className={`m-0 ${e.toLowerCase() === 'a' ? 'text-blue-800 lowercase text-lg' : 'text-black'}`}>
+          <span key={i} className={`m-0 ${e.toLowerCase() === 'a' ? 'text-blue-800 lowercase text-lg lg:text-xl' : 'text-black'}`}>
             {e}
           </span>
         ))}
       </div>
-      <div className='flex flex-row items-center gap-10 lg:gap-20 relative'>
-        <button className='flex flex-row items-center gap-1.5 py-1 px-2 cursor-pointer group' onClick={() => setLanguageSelectVis(p => p == true ? false : true)}>
+      <div className='flex flex-row items-center gap-10 lg:gap-20 relative lg:text-lg'>
+        <button className='flex flex-row items-center gap-1.5 py-1 lg:py-2 px-2 cursor-pointer group' onClick={() => setLanguageSelectVis(p => p == true ? false : true)}>
           <img src={languageDict[language].flag} alt="" className='w-8'/>
           <p className='group-hover:text-blue-800 transition'>{languageDict[language].language}</p>
           <ChevronDown />
@@ -300,7 +300,7 @@ const Header = ({ setNavVis, language, languageSelectVis, setLanguageSelectVis, 
 
 const Navbar = ({ setNavVis, handleNavClick, navVis, sections }) => {
   return (
-    <motion.div className='fixed bg-black/95 top-0 bottom-0 right-0 left-40 z-101 flex flex-col p-6 px-4 pl-6 items-end gap-16' key={navVis}
+    <motion.div className='fixed bg-black/95 top-0 bottom-0 right-0 left-40 md:left-100 z-101 flex flex-col p-6 px-4 pl-6 items-end gap-16' key={navVis}
       initial={{ x: '100%' }}
       animate={{ x: 0 }}
       exit={{ x: '100%' }}
@@ -320,19 +320,19 @@ const Navbar = ({ setNavVis, handleNavClick, navVis, sections }) => {
 
 const Hero = ({ language }) => {
   return (
-    <section className='relative w-full h-[312px] lg:h-[556px] text-white flex flex-col items-center justify-center text-center px-6 gap-1 lg:gap-3 scroll-mt-[64px]' id='hero'>
+    <section className='relative w-full h-[312px] md:h-[470px] lg:h-[556px] xl:h-[612px] text-white flex flex-col items-center justify-center text-center px-6 gap-1 lg:gap-3 xl:gap-5 scroll-mt-[64px]' id='hero'>
       <div className='bg-blue-800/66 inset-0 absolute -z-1'></div>
       <img src={heroImg} alt="" className='absolute inset-0 -z-2 w-full h-full object-cover'/>
-      <h1 className='font-bold text-lg lg:text-4xl'>{languageText[language].hero.header}</h1>
-      <p className='text-sm lg:text-xl font-light'>{languageText[language].hero.paragraph}</p>
+      <h1 className='font-bold text-lg md:text-3xl lg:text-4xl xl:text-5xl'>{languageText[language].hero.header}</h1>
+      <p className='text-sm md:text-lg lg:text-xl xl:text-2xl font-light'>{languageText[language].hero.paragraph}</p>
     </section>
   )
 }
 
 const Showcase = ({ language, lightboxVis, setLightboxVis, imgIndex, setImgIndex, handleLightboxOpen }) => {
   return (
-    <section className='px-4 lg:px-12 py-20 scroll-mt-[64px] text-center' id='showcase'>
-      <h2 className='font-bold text-[2.5rem] lg:text-[3.2rem] mb-10'>{languageText[language].showcase.header}</h2>
+    <section className='px-4 lg:px-12 py-20 scroll-mt-[64px] text-center md:px-10 xl:px-20' id='showcase'>
+      <h2 className='font-bold text-[2.5rem] md:text-[3rem] lg:text-[3.2rem] xl:text-[3.6rem] mb-10'>{languageText[language].showcase.header}</h2>
       <div className='grid grid-cols-2 grid-rows-5 lg:grid-cols-3 lg:grid-rows-4 xl:grid-cols-4 xl:grid-rows-3 gap-y-8 gap-x-4'>
         {images.map((e, i) => {
           return (
@@ -349,15 +349,19 @@ const Showcase = ({ language, lightboxVis, setLightboxVis, imgIndex, setImgIndex
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
             >
-              <div className='w-full flex flex-row justify-end pr-2 lg:text-lg'>
+              <div className='w-full max-w-2xl xl:max-w-3xl 2xl:max-w-4xl flex flex-row justify-end md:text-lg'>
                 <button onClick={() => setLightboxVis(false)} className='bg-zinc-700 p-2 text-zinc-400 flex justify-center items-center rounded-full hover:text-blue-400 transition cursor-pointer'>
                   <Close />
                 </button>
               </div>
-              <div className='h-[228px] lg:h-[368px] rounded-md overflow-hidden'>
-                <img src={images[imgIndex].img} alt="" className='w-full h-full object-cover'/>
+              <div className='rounded-md overflow-hidden max-h-[60vh] lg:max-h-[70vh] max-w-full flex items-center justify-center'>
+                <img
+                  src={images[imgIndex].img}
+                  alt=""
+                  className='max-h-[70vh] max-w-full w-auto h-auto object-contain'
+                />
               </div>
-              <div className='w-full flex flex-row text-zinc-400 items-center justify-center gap-4 lg:gap-6 lg:text-lg'>
+              <div className='w-full flex flex-row text-zinc-400 items-center justify-center gap-4 md:gap-6 md:text-lg'>
                 <button onClick={() => setImgIndex(p => p == 0 ? 11 : p-1)}>
                   <FontAwesomeIcon icon={faArrowLeft} className='p-1.5 py-2 bg-zinc-700 rounded-full hover:text-blue-400 transition cursor-pointer'/>
                 </button>
@@ -369,7 +373,7 @@ const Showcase = ({ language, lightboxVis, setLightboxVis, imgIndex, setImgIndex
           )}
         </AnimatePresence>
       </div>
-      <div className='flex justify-center pt-18 lg:px-48'>
+      <div className='flex justify-center pt-18 xl:pt-30 md:px-24 lg:px-40 xl:px-52'>
         <div className='relative w-full aspect-video'>
           <iframe 
           src="https://www.youtube.com/embed/m4B7nXNl0Ss" 
@@ -393,20 +397,20 @@ const PriceList = ({ language, tableVis, setTableVis }) => {
   console.log('kategorie', tableData.table.people.header)
 
   return (
-    <section className='py-12 pb-36 px-6 flex flex-col items-center scroll-mt-[64px]' id='priceList'>
-      <h2 className='font-bold text-[2.5rem] lg:text-[3.2rem] mb-14'>{tableData.header}</h2>
-      <div className='flex flex-row gap-6 mb-10 border-b-2 border-t-2 font-medium px-4'>
+    <section className='py-12 pb-36 px-6 flex flex-col items-center scroll-mt-[64px] md:px-26 lg:px-32 xl:px-70 2xl:px-90' id='priceList'>
+      <h2 className='font-bold text-[2.5rem] md:text-[3rem] lg:text-[3.2rem] xl:text-[3.6rem] mb-14'>{tableData.header}</h2>
+      <div className='flex flex-row gap-6 md:gap-10 lg:gap-14 xl:gap-18 mb-10 lg:mb-14 border-b-2 border-t-2 font-medium px-4'>
         {tableData.table.categories.map((e, i) => (
-            <button onClick={() => setTableVis(i)} className={`${tableVis == i ? "border-b-3 border-blue-600" : "border-b-3 border-white"} cursor-pointer py-4 pt-5`}>
+            <button onClick={() => setTableVis(i)} className={`${tableVis == i ? "border-b-3 border-blue-600" : "border-b-3 border-white"} cursor-pointer py-4 pt-5 md:text-lg xl:text-xl`}>
               {e.category.header}
             </button>
         ))}
       </div>
-      <table className='table-auto border-collapse border lg:text-xl w-full'>
-        <thead>
+      <table className='table-auto border-collapse border md:text-lg lg:text-xl w-full'>
+        <thead className='text-lg md:text-xl lg:text-2xl xl:text-3xl'>
           <tr>
-            <th className='border py-4'>{tableData.service}</th>
-            <th className='border py-4'>{tableData.price}</th>
+            <th className='border py-4 lg:py-6 xl:py-8'>{tableData.service}</th>
+            <th className='border py-4 lg:py-6 xl:py-8'>{tableData.price}</th>
           </tr>
         </thead>
         <tbody>
@@ -414,8 +418,8 @@ const PriceList = ({ language, tableVis, setTableVis }) => {
             <>
               {kategorie[0].category.services.map((e, i) => (
                   <tr>
-                    <td className='border p-4'>{e.service}</td>
-                    <td className='border p-4'>{e.price}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.service}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.price}</td>
                   </tr>
               ))}
             </>
@@ -424,8 +428,8 @@ const PriceList = ({ language, tableVis, setTableVis }) => {
             <>
               {kategorie[1].category.services.map((e, i) => (
                   <tr>
-                    <td className='border p-4'>{e.service}</td>
-                    <td className='border p-4'>{e.price}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.service}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.price}</td>
                   </tr>
               ))}
             </>
@@ -434,27 +438,27 @@ const PriceList = ({ language, tableVis, setTableVis }) => {
             <>
               {kategorie[2].category.services.map((e, i) => (
                   <tr>
-                    <td className='border p-4'>{e.service}</td>
-                    <td className='border p-4'>{e.price}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.service}</td>
+                    <td className='border p-4 lg:py-6 xl:py-8'>{e.price}</td>
                   </tr>
               ))}
             </>
           )}
         </tbody>
       </table>
-      <h3 className='font-bold text-[2rem] lg:text-[3.2rem] mt-14 mb-10 border-b-2 border-t-2 py-2 px-8'>{tableData.table.people.header}</h3>
-      <table className='table-auto border-collapse border lg:text-xl w-full'>
-        <thead>
+      <h3 className='font-bold text-[2rem] md:text-[2.5rem] lg:text-[3.2rem] xl:text-[3.6rem] mt-14 lg:mt-28 mb-10 lg:mb-14 border-b-2 border-t-2 py-2 px-8'>{tableData.table.people.header}</h3>
+      <table className='table-auto border-collapse border md:text-lg lg:text-xl w-full'>
+        <thead className='text-lg md:text-xl lg:text-2xl xl:text-3xl'>
           <tr>
-            <th className='border py-4'>{tableData.table.people.age}</th>
-            <th className='border py-4'>{tableData.table.people.price}</th>
+            <th className='border p-4 lg:py-6 xl:py-8'>{tableData.table.people.age}</th>
+            <th className='border p-4 lg:py-6 xl:py-8'>{tableData.table.people.price}</th>
           </tr>
         </thead>
         <tbody>
           {tableData.table.people.prices.map((e, i) => (
               <tr>
-                <td className='border p-4'>{e.age}</td>
-                <td className='border p-4'>{e.price}</td>
+                <td className='border p-4 lg:py-6 xl:py-8'>{e.age}</td>
+                <td className='border p-4 lg:py-6 xl:py-8'>{e.price}</td>
               </tr>
           ))}
         </tbody>
@@ -465,10 +469,10 @@ const PriceList = ({ language, tableVis, setTableVis }) => {
 
 const Footer = ({ language }) => {
   return (
-    <footer className='bg-blue-800 w-full py-12 pt-8 pb-8 px-6 lg:px-18 flex flex-col lg:flex-row lg:justify-between lg:items-center gap-10 scroll-mt-[64px] text-center' id='footer'>
-      <div className='flex flex-col gap-10 lg:text-center lg:items-center lg:justify-center'>
-        <h2 className='font-bold text-[2rem] lg:text-[2.6rem] text-white'>{languageText[language].footer.header}</h2>
-        <div className='flex flex-col gap-3 items-center lg:text-lg'>
+    <footer className='bg-blue-800 w-full py-12 pt-8 pb-8 px-10 flex flex-col lg:flex-row lg:justify-between lg:items-center gap-10 xl:gap-16 scroll-mt-[64px] text-center md:px-20 lg:px-30 xl:px-45 xl:py-20' id='footer'>
+      <div className='flex flex-col gap-10 xl:gap-14 lg:text-center lg:items-center lg:justify-center'>
+        <h2 className='font-bold text-[2rem] md:text-[2.3rem] lg:text-[2.6rem] xl:text-[3rem] 2xl:text-[3.3rem] text-white'>{languageText[language].footer.header}</h2>
+        <div className='flex flex-col gap-3 xl:gap-5 items-center md:text-lg xl:text-xl 2xl:text-2xl'>
           <span className='text-white flex flex-row items-center gap-2'>
             <FontAwesomeIcon icon={faLocationDot}/>
             <p>Chľaba 752</p>
@@ -483,8 +487,8 @@ const Footer = ({ language }) => {
           </span>
         </div>
       </div>
-      <div className='flex justify-center border-3 border-white lg:w-full lg:ml-24'>
-        <div className='relative w-full aspect-video'>
+      <div className='flex justify-center lg:w-full lg:ml-24 2xl:ml-95'>
+        <div className='relative w-full aspect-video border-3 border-white'>
           <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21430.21820753928!2d18.803142899999997!3d47.8245335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476a890023d8af8b%3A0x130435ac01181746!2zQ2FtcCBDaMS-YWJhLUtvdsOhxI1vdg!5e0!3m2!1ssk!2ssk!4v1785247708408!5m2!1ssk!2ssk"   
             title="maps"
@@ -516,6 +520,18 @@ function App() {
       setLanguage(localLanguage)
     }
   }, []);
+
+  useEffect(() => {
+  if (lightboxVis) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+
+  return () => {
+    document.body.style.overflow = ''; // cleanup on unmount too
+  };
+}, [lightboxVis]);
 
   const handleNavClick = () => {
     setNavVis(false)
