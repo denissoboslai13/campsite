93b3c7fcb5cd8f3d2e4dd1c1eb102225c8213ca4
denissoboslai13 <soboslaid@gmail.com>
2026-08-06 18:02:32 +0200

    og testt

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
-import heroImg from '../assets/camp_pictures/heroImg.webp'
+// import heroImg from '../assets/camp_pictures/heroImg.webp'
+import heroPlaceholder from '../assets/camp_pictures/og-image.png'
 
 import { languageText } from './languageData'
 import type { Language } from './languageData'
@@ -11,7 +12,7 @@ export const Hero = ({ language }: Props) => {
   return (
     <section className='relative w-full h-78 md:h-117.5 lg:h-139 xl:h-153 3xl:min-h-189 text-white flex flex-col items-center justify-center text-center px-6 gap-1 lg:gap-3 xl:gap-5 scroll-mt-16' id='hero'>
       <div className='bg-blue-800/66 inset-0 absolute -z-1'></div>
-      <img src={heroImg} alt="Hero image of campground" className='absolute inset-0 -z-2 w-full h-full object-cover'/>
+      <img src={heroPlaceholder} alt="Hero image of campground" className='absolute inset-0 -z-2 w-full h-full object-cover'/>
       <h1 className='font-bold text-lg md:text-3xl lg:text-4xl xl:text-5xl'>{languageText[language].hero.header}</h1>
       <p className='text-sm md:text-lg lg:text-xl xl:text-2xl font-light'>{languageText[language].hero.paragraph}</p>
     </section>
